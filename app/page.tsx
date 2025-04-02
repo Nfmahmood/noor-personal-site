c2122b86f6bdfc90f4772e1a0a0d6fe958e6be1a
Noor Fatima <noorfatima@Noors-MacBook-Air.local>
2025-04-02 12:09:12 +0500

    updating data

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,51 @@
 export default function Page() {
+  const myProjects = [
+    {
+      title: "My To-Do App",
+      url: "https://to-do-list-umber-eta-45.vercel.app/",
+    },
+    {
+      title: "Learn Javascript Essential",
+      url: "https://accordion-three-lake.vercel.app/",
+    },
+    {
+      title: "Tip Calculator",
+      url: "https://tip-calculator-livid-gamma.vercel.app/",
+    },
+    {
+      title: "Flash Cards",
+      url: "https://study-flashcards-two.vercel.app/",
+    },
+    {
+      title: "Split Bill",
+      url: "https://split-bill-flame.vercel.app/",
+    },
+    {
+      title: "Date Counter",
+      url: "https://date-counter-pied.vercel.app/",
+    },
+    {
+      title: "Travel Packing List",
+      url: "https://travel-list-livid-three.vercel.app/",
+    },
+    {
+      title: "Steps to Success",
+      url: "https://steps-to-success.vercel.app/",
+    },
+    {
+      title: "Get Advice",
+      url: "https://get-advice-six.vercel.app/",
+    },
+    {
+      title: "Pizza Menu",
+      url: "https://pizza-menu-pink.vercel.app/",
+    },
+  ];
+
+  myProjects.map((project) => {
+    console.log(project.title.toUpperCase());
+  });
+
   return (
     <section>
       <h1 className="mb-8 text-xl font-semibold tracking-tighter">About</h1>
@@ -9,113 +56,24 @@ export default function Page() {
         {`Starting from scratch hasn’t been easy, but I’ve embraced every challenge with determination, learning relentlessly and pushing myself to grow in order to build something meaningful. Now, as I take this next big step—landing my first job as a web developer—I bring not just technical skills, but resilience, adaptability, and a unique perspective shaped by my journey. I’m ready to contribute, learn, adapt and prove that bold decisions lead to incredible opportunities.
 `}
       </p>
+
       <h2 className="mt-[3rem] mb-8 text-xl font-semibold tracking-tighter">
         My Portfolio
       </h2>
-      <p className="mb-4 font-light">
-        {`Building dynamic, user-friendly web applications with clean, efficient code. Check out my projects below!`}
-      </p>
-      <div className="my-8">
+
+      <div>
         <ul>
-          <li className="mt-[0.5rem]">
-            <a
-              href="https://to-do-list-umber-eta-45.vercel.app/"
-              className="text-neutral-900 dark:text-neutral-100 italic tracking-tight"
-              target="_blank"
-            >
-              My To-Do App
-            </a>
-          </li>
-
-          <li className="mt-[0.5rem]">
-            <a
-              href="https://accordion-three-lake.vercel.app/"
-              className="text-neutral-900 dark:text-neutral-100 tracking-tight italic"
-              target="_blank"
-            >
-              Learn Javascript Essentials
-            </a>
-          </li>
-
-          <li className="mt-[0.5rem]">
-            <a
-              href="https://tip-calculator-livid-gamma.vercel.app/"
-              className="text-neutral-900 dark:text-neutral-100 tracking-tight italic"
-              target="_blank"
-            >
-              Tip Calculator
-            </a>
-          </li>
-
-          <li className="mt-[0.5rem]">
-            <a
-              href="https://study-flashcards-two.vercel.app/"
-              className="text-neutral-900 dark:text-neutral-100 tracking-tight italic"
-              target="_blank"
-            >
-              Study Flash Cards
-            </a>
-          </li>
-
-          <li className="mt-[0.5rem]">
-            <a
-              href="https://split-bill-flame.vercel.app/"
-              className="text-neutral-900 dark:text-neutral-100 tracking-tight italic"
-              target="_blank"
-            >
-              Split Bill
-            </a>
-          </li>
-
-          <li className="mt-[0.5rem]">
-            <a
-              href="https://date-counter-pied.vercel.app/"
-              className="text-neutral-900 dark:text-neutral-100 tracking-tight italic"
-              target="_blank"
-            >
-              Date Counter
-            </a>
-          </li>
-
-          <li className="mt-[0.5rem]">
-            <a
-              href="https://travel-list-livid-three.vercel.app/"
-              className="text-neutral-900 dark:text-neutral-100 tracking-tight italic"
-              target="_blank"
-            >
-              Travel Packing List
-            </a>
-          </li>
-
-          <li className="mt-[0.5rem]">
-            <a
-              href="https://steps-to-success.vercel.app/"
-              className="text-neutral-900 dark:text-neutral-100 tracking-tight italic"
-              target="_blank"
-            >
-              Steps to Success
-            </a>
-          </li>
-
-          <li className="mt-[0.5rem]">
-            <a
-              href="https://get-advice-six.vercel.app/"
-              className="text-neutral-900 dark:text-neutral-100 tracking-tight italic"
-              target="_blank"
-            >
-              Get Advice
-            </a>
-          </li>
-
-          <li className="mt-[0.5rem]">
-            <a
-              href="https://pizza-menu-pink.vercel.app/"
-              className="text-neutral-900 dark:text-neutral-100 tracking-tight italic"
-              target="_blank"
-            >
-              Pizza Menu
-            </a>
-          </li>
+          {myProjects.map((project) => (
+            <li className="mt-[0.5rem]">
+              <a
+                href={project.url}
+                className="text-neutral-900 dark:text-neutral-100 italic tracking-tight underline"
+                target="_blank"
+              >
+                {project.title}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </section>
