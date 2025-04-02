cad89f0e693705abb61ff7c4d9f69d281a545765
Noor Fatima <noorfatima@Noors-MacBook-Air.local>
2025-04-02 12:44:48 +0500

    fixing blog error

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,10 @@
-export const baseUrl = "https://noor-personal-site.vercel.app/";
+export const baseUrl = "https://noor-personal-site.vercel.app";
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }));
-
-  let routes = ["", "/blog"].map((route) => ({
+  let routes = [""].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...blogs];
+  return [...routes];
 }
